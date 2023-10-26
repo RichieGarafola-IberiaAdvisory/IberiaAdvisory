e4f7829a72791db859c1ba15334e2975869f62d2
RichieGarafola-IberiaAdvisory <135731462+RichieGarafola-IberiaAdvisory@users.noreply.github.com>
2023-10-26 11:12:57 -0400

    Add files via upload

---
+++ home.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+# Set the title and page description
+st.title("Welcome to IberiaAdvisory's Home")
+st.markdown("This is the home for IberiaAdvisory's proprietary tools for automating tasks.")
+
+# Home page content
+st.write("Welcome to IberiaAdvisory's Home, the central hub for our exclusive tools designed to automate various processes.")
+
+st.write("Explore the following sections to access our suite of powerful automation tools:")
+
+# Section for Tool 1
+st.header("Tool 1: Tripwire Tracker")
+st.write("The Tripwire Tracker App is a Python script designed to process data from Excel files and filter information based on specific criteria. It leverages the Pandas library to manipulate data efficiently.")
+
+# Section for Tool 2
+st.header("Tool 2: Subcontractor Invoice Review")
+st.write("The Subcontractor Invoice Review App is a Python script designed to process data from Excel files and filter information based on specific criteria. It leverages the Pandas library to manipulate data efficiently.")
+
+st.markdown("---")
+# Footer
+st.write("If you have any questions or need assistance, please feel free to contact Richie RGarafola@IberiaAdvisory.com")
